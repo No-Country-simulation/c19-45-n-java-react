d25838521b7fb4f05c659c03722939b62efe5826
EdgarCamberos1894 <edgarcamberos18@gmail.com>
2024-07-03 19:19:47 -0700

    feat(backend): configuración inicial del proyecto backend
    - Inicializar la aplicación Spring Boot.
    - Configurar la estructura básica del proyecto.
    - Incluir dependencias iniciales del backend.

---
+++ backend/petConnect/src/test/java/com/noCountry/petConnect/PetConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noCountry.petConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
