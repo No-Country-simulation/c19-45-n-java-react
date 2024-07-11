0cf21c4930d997aedb11086c89820222eaff85c8
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2024-07-11 18:17:36 -0500

    creacion de nav

--- frontend/src/components/layouts/SiteHeader.tsx
+++ frontend/src/components/layouts/SiteHeader.tsx
@@ -5,11 +5,11 @@ import MobileNav from './MobileNav';
 
 export default function SiteHeader() {
 	return (
-		<header className="sticky top-0 z-50 w-full border-b bg-headerBg-orangeHeader">
+		<header className="sticky top-0 z-50 w-full border-b bg-background">
 			<div className="container flex h-16 items-center">
 				<div className="flex items-center gap-8">
-					<Link href="/" className="hidden items-center space-x-2 lg:flex">
-						<span className="hidden font-bold lg:inline-block">PetFriendly</span>
+					<Link href="/" className=" items-center space-x-2 lg:flex">
+						<span className=" text-2xl font-bold ">PetFriendly</span>
 						<span className="sr-only">Home</span>
 					</Link>
 					<MainNav />
