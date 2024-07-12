775b5ad28640ee929307ef6222263b4ab130768b
Nataly Rojas <natalyrojasm5@gmail.com>
2024-07-11 19:10:06 -0500

    feat: layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,12 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"
-import { cn } from "@/lib/utils"
+import { Inter as FontSans } from "next/font/google";
+// import { cn } from "@/lib/utils"
 import "./globals.css";
-
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,13 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
