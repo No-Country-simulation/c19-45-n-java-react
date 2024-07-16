f15ed6d8c3629c33a50955c6b33e1bb7fff905a7
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2024-07-16 14:23:20 -0500

    Configuracion de iamge

--- frontend/src/app/pet-detail/[petId]/page.tsx
+++ frontend/src/app/pet-detail/[petId]/page.tsx
@@ -2,11 +2,11 @@ import PetDetail from './_components/PetDetail';
 
 interface PetDetailPageProps {
 	params: {
-		ppetId: string,
+		petId: string,
 	},
 }
 export default function PetDetailPage({ params }: PetDetailPageProps) {
-    
+
 	return (
 		<div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
 			<div className="max-w-4xl bg-white rounded-xl shadow-md overflow-hidden w-full p-4 md:p-8">
