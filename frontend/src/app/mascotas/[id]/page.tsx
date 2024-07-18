7ce078a2d8a62a4307399c1dd705a64dbf1474a7
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2024-07-17 22:55:08 -0500

    Se procede a realizar los ajustes al detalle de la mascota en la ruta app/mascotas/[id]

--- frontend/src/app/mascotas/[id]/page.tsx
+++ frontend/src/app/mascotas/[id]/page.tsx
@@ -1,7 +1,14 @@
-import { ViewPetDetail } from "@/modules/pets/views";
+import { CardPetDetail } from '@/modules/pets/components';
+import { ViewPetDetail } from '@/modules/pets/views';
 
 const PagePet = () => {
-  return <ViewPetDetail />;
+	return (
+		<div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
+			<div className="max-w-4xl bg-white rounded-xl shadow-md overflow-hidden w-full p-4 md:p-8">
+				<CardPetDetail />;
+			</div>
+		</div>
+	);
 };
 
 export default PagePet;
