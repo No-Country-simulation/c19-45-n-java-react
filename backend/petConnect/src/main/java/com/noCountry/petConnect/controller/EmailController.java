568a23b08f83b6d8ad20d4940245fd8775d5c163
EdgarCamberos1894 <edgarcamberos18@gmail.com>
2024-07-21 17:15:14 -0700

    eliminar libreria que no se utiliza

--- backend/petConnect/src/main/java/com/noCountry/petConnect/controller/EmailController.java
+++ backend/petConnect/src/main/java/com/noCountry/petConnect/controller/EmailController.java
@@ -14,7 +14,6 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
