dae976c38c6613e8d6747397517bce87e5cdb59d
conrado <carlos_ag20@hotmail.com>
2024-07-21 21:24:33 -0300

    feature/petsWelcome

--- frontend/src/modules/pets/views/PetsWelcome.tsx
+++ frontend/src/modules/pets/views/PetsWelcome.tsx
@@ -1,28 +1,29 @@
 import Image from "next/image";
 
-
 export const ViewPetsWelcome = () => {
   return (
-    <div className="hero  bg-yellow-200 min-h-screen bg-cover bg-center">
-       <Image
-      src={"/imagen.jpg"}
-      width={1440}
-      height={1440}
-      alt="Picture of the author"
-      />
+    <div className="hero  bg-[#FFAC31] min-h-screen bg-cover bg-center">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold text-black mb:text-4xl  ">
+          <Image
+            className="mt-20"
+            src={"/imagen-01.png"}
+            width={400}
+            height={400}
+            alt="Picture of the author"
+          />
+          <h1 className="text-4xl font-bold text-black   ">
             Adopta a tu mejor amig@
           </h1>
-          <p className="py-6 text-black font-bold">
-            Bienvenidos a nuestra <span>comunidad</span> de amantes de los<br />
+          <p className="py-6 text-black text-sl">
+            Bienvenidos a nuestra <span className="underline font-bold">comunidad</span> de amantes de los
+            <br />
             animales.Aquí, encontraras adorables mascotas <br />
             que buscan un hogar lleno de amor.
           </p>
-          <button className="btn btn-primary">Crear cuenta</button>
-          <p className="py-6 text-black font-bold">ya tengo una cuenta</p>
-         
+          <button className="btn btn-primary text-2xl m-6">Crear cuenta</button>
+          <a href="#" className="block w-full  underline text-3xl text-black ">ya tengo una cuenta</a>
+          
         </div>
       </div>
     </div>
