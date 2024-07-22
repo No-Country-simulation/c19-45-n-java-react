17a685786e93eae62dd65e6b05f4119213d45f2b
EdgarCamberos1894 <edgarcamberos18@gmail.com>
2024-07-21 17:11:56 -0700

    eliminar libreria que no se utiliza

--- backend/petConnect/src/main/java/com/noCountry/petConnect/controller/PerfilUsuarioController.java
+++ backend/petConnect/src/main/java/com/noCountry/petConnect/controller/PerfilUsuarioController.java
@@ -13,11 +13,10 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+
 
 @Tag(name = "Perfil Usuario")
 @SecurityRequirement(name = "bearer-key")
