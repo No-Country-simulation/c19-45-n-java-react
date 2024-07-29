2f7db0a59df95feff7238fabdb53f96e3155edc9
Nataly Rojas <natalyrojasm5@gmail.com>
2024-07-29 16:20:24 -0500

    ⚡️ feat: modal member finish

--- frontend/src/app/_components/Member.tsx
+++ frontend/src/app/_components/Member.tsx
@@ -1,3 +1,5 @@
+import classNames from "classnames";
+import Image from "next/image";
 import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io5";
 
 interface IMember {
@@ -5,19 +7,44 @@ interface IMember {
   role: string;
   github: string;
   linkedin: string;
+  image?: string;
+  active?: boolean;
 }
 
-export const Member = ({ name, role, github, linkedin }: IMember) => {
+export const Member = ({
+  name,
+  role,
+  github,
+  linkedin,
+  image,
+  active,
+}: IMember) => {
+  const classes = classNames(
+    "bg-[#FFCD82] p-6 rounded-lg shadow-lg w-full flex flex-col items-center"
+  );
   return (
-    <div className="bg-[#FFCD82] p-6 rounded-lg shadow-lg w-full flex flex-col items-center">
+    <div className={classes}>
       <div className="avatar mb-4">
         <div className="w-20 rounded-full">
-          <img src="https://placehold.co/80x80" />
+          <Image
+            src={image ? `/members/${image}` : "/members/avatar.jpg"}
+            width={80}
+            height={80}
+            alt={name}
+          />
         </div>
       </div>
       <div className="flex gap-4 items-center">
-        {github && <IoLogoGithub size={35} className="cursor-pointer" />}
-        {linkedin && <IoLogoLinkedin size={35} className="cursor-pointer" />}
+        {github && (
+          <a href={github} target="_blank">
+            <IoLogoGithub size={35} className="cursor-pointer" />
+          </a>
+        )}
+        {linkedin && (
+          <a href={linkedin} target="_blank">
+            <IoLogoLinkedin size={35} className="cursor-pointer" />
+          </a>
+        )}
       </div>
       <div className="p-4 text-center">
         <p className="text-2xl font-semibold">{name}</p>
